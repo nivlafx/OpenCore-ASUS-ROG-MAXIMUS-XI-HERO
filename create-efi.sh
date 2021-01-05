51f9ae2fe9532c78fafa82d7ee1acdf57e8cec1b
Alvin Francis <nivlafx@gmail.com>
2021-01-05 19:31:10 -0400

    Correct location for coreutils on MacPorts

--- create-efi.sh
+++ create-efi.sh
@@ -9,7 +9,7 @@ set -euf -o pipefail
 export LC_ALL="en_US.UTF-8"
 
 # Extend PATH to always use Coreutils supplied utilities first.
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+export PATH="/opt/local/libexec/gnubin:${PATH}"
 
 # Directories
 readonly BASE_DIR="$(dirname "$(realpath "$0")")"
